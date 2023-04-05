344bbc6b72fb0543007a10d38c7233cc37f56291
Gaurav <gauravp@zignuts.com>
2023-04-05 19:23:35 +0530

    added column expirydate

--- database/migrations/2014_10_12_100000_create_password_reset_tokens_table.php
+++ database/migrations/2014_10_12_100000_create_password_reset_tokens_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('password_reset_tokens', function (Blueprint $table) {
             $table->string('email')->primary();
             $table->string('token');
+            $table->timestamp('expiry_date');
             $table->timestamp('created_at')->nullable();
         });
     }
