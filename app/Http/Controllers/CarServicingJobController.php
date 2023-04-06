ca72f44976792b6f7bd19774c3cdc8ffaf47e696
Gaurav <gauravp@zignuts.com>
2023-04-06 19:23:26 +0530

    list assign and update job

--- app/Http/Controllers/CarServicingJobController.php
+++ app/Http/Controllers/CarServicingJobController.php
@@ -2,9 +2,47 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\CarServicing;
+use App\Models\CarServicingJob;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class CarServicingJobController extends Controller
 {
-    //
+    public function list()
+    {
+        $carServicing = CarServicing::all();
+        return ok('Car Servicing',$carServicing);
+    }
+
+    public function assign(Request $request)
+    {
+        $validation = Validator::make($request->all(),[
+            'car_servicing_id' => 'required|exists:car_servicings,id',
+            'mechanic_id'      => 'required|exists:mechanics,id',
+            'service_type_id'  => 'required|exists:service_types,id'
+        ]);
+
+        if($validation->fails())
+            return error('Validation Error',$validation->errors(),'Validation');
+        
+        CarServicingJob::create($request->only(['car_servicing_id','mechanic_id','service_type_id'])+[
+            'status'    =>  'pending'
+        ]);
+    }
+
+    public function update($id,Request $request)
+    {
+        $validation = Validator::make($request->all(),[
+            'status'    => 'required|in:Pending,In-Progress,Complete'
+        ]);
+
+        if($validation->fails())
+            return error('Validation',$validation->errors(),'validation');
+        
+        $carServicingJob = CarServicingJob::findOrFail($id);
+        
+        $carServicingJob->update($request->only('status'));
+        return ok('Status Updated Successfully');
+    }
 }
