90f9b268e56595c7e6e866190b9110de0ac5133b
Gaurav <gauravp@zignuts.com>
2023-04-06 17:58:59 +0530

    fillable and relationship

--- app/Models/Garage.php
+++ app/Models/Garage.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Garage extends Model
 {
     use HasFactory;
+    protected $fillable = ['city_id', 'state_id', 'country_id', 'owner_id', 'name', 'address1', 'address2', 'zip_code'];
+
+    public function serviceTypes()
+    {
+        return $this->belongsToMany(ServiceType::class, 'garage_service_types', 'garage_id', 'service_type_id');
+    }
 }
