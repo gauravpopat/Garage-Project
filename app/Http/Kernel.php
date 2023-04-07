d897a4fbe5bad36f9e3676a1123cf412ecd67a87
Gaurav <gauravp@zignuts.com>
2023-04-07 18:45:57 +0530

    added hasOwner and hasMechanic in middleware alias

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,6 +63,7 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'hasAccess' => \App\Http\Middleware\HasAccess::class
+        'hasOwner' => \App\Http\Middleware\hasOwner::class,
+        'hasMechanic'   => \App\Http\Middleware\hasMechanic::class,
     ];
 }
