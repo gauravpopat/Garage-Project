57567edca89210a059829d2c43ec15ef75575b31
Gaurav <gauravp@zignuts.com>
2023-04-07 18:43:36 +0530

    crud

--- app/Http/Controllers/CarController.php
+++ app/Http/Controllers/CarController.php
@@ -3,8 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Models\Car;
-use App\Models\CarServicing;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
 
 class CarController extends Controller
@@ -13,11 +13,8 @@ class CarController extends Controller
     {
         $validation = Validator::make($request->all(), [
             'company_name'          => 'required',
-            'model_name'            => 'required',
+            'model_name'            => 'required|unique:cars,model_name',
             'manufacturing_year'    => 'required|digits:4|before:' . (date('Y') + 1),
-            'garage_id'             => 'required|exists:garages,id',
-            'service_id'            => 'required|exists:service_types'
-
         ]);
 
         if ($validation->fails())
@@ -27,11 +24,74 @@ class CarController extends Controller
             'owner_id'  => auth()->user()->id,
         ]);
 
-        CarServicing::create($request->only(['garage_id', 'service_id']) + [
-            'car_id'    => $car->id,
-            'status'    => 'Initiated',
+        return ok('Car Added Successfully', $car);
+    }
+
+    public function addToService(Request $request)
+    {
+        $validation = Validator::make($request->all(), [
+            'car_id'        => 'required|exists:cars,id',
+            'garage_id'     => 'required|exists:garages,id',
+            'service_id'    => 'required|array|exists:service_types,id'
         ]);
 
-        return ok('Car Goes to the Service.', $car);
+        if ($validation->fails())
+            return error('Validation Error', $validation->errors(), 'validation');
+        
+        $car = Car::find($request->car_id);
+        $carOwnerId = $car->user->id;
+
+        if($carOwnerId != auth()->user()->id){
+            return error('Not your car.');
+        }
+
+        // return 'pending task';
+    }
+
+    public function update($id, Request $request)
+    {
+        $car = Car::find($id);
+        $carOwnerId = $car->user->id;
+
+        if (($carOwnerId != auth()->user()->id)) {
+            return error('Not your car.');
+        }
+
+        $validation = Validator::make($request->all(), [
+            'company_name'          => 'required',
+            'model_name'            => 'required',
+            'manufacturing_year'    => 'required|digits:4|before:' . (date('Y') + 1),
+        ]);
+
+        if ($validation->fails())
+            return error('Validation Error', $validation->errors(), 'validation');
+
+        $car->update($request->all());
+        return ok('Car Updated Successfully.');
+    }
+
+    public function show($id)
+    {
+        $car = Car::find($id);
+        $carOwnerId = $car->user->id;
+
+        if ($carOwnerId == auth()->user()->id) {
+            return ok('Car Detail.', $car);
+        } else {
+            return error('Car Not Found');
+        }
+    }
+
+    public function delete($id)
+    {
+        $car = Car::find($id);
+        $carOwnerId = $car->user->id;
+
+        if ($carOwnerId == auth()->user()->id) {
+            $car->delete();
+            return ok('Car Deleted Successfully');
+        } else {
+            return error('Car Not Found');
+        }
     }
 }
