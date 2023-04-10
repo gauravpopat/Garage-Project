248c1b4b388118a20c967f76eaa2d6f08f36c698
Gaurav <gauravp@zignuts.com>
2023-04-10 15:05:54 +0530

    getHistory

--- app/Http/Controllers/CarServicingController.php
+++ app/Http/Controllers/CarServicingController.php
@@ -6,8 +6,10 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use App\Models\Car;
 use App\Models\CarServicing;
+use App\Models\CarServicingJob;
 use App\Models\Garage;
 use App\Traits\ListingApiTrait;
+use App\Models\User;
 
 class CarServicingController extends Controller
 {
@@ -16,7 +18,7 @@ class CarServicingController extends Controller
     {
         $this->ListingValidation();
         $query = Garage::query();
-        $searchable_fields = ['city_id', 'state_id', 'country_id', 'name', 'address1', 'address2', 'zip_code'];
+        $searchable_fields = ['city_id', 'state_id', 'country_id', 'name'];
         $data = $this->filterSearchPagination($query, $searchable_fields);
         return ok('Garage List', [
             'garages'   =>  $data['query']->get(),
@@ -39,10 +41,54 @@ class CarServicingController extends Controller
         $carOwnerId = $car->user->id;
 
         if ($carOwnerId == auth()->user()->id) {
-            CarServicing::create($request->all());
+            $car->carServicings()->attach($request->only(['garage_id', 'service_id']));
             return ok('Car Added For Service', $car);
         } else {
             return error('Car Not Found');
         }
+    }
+
+    public function getHistory(Request $request)
+    {
+        $validation = Validator::make($request->all(), [
+            'car_id'    => 'required|exists:cars,id'
+        ]);
+
+        if ($validation->fails())
+            return error('Validation Error', $validation->errors(), 'validation');
+
+        $carServicing = CarServicing::where('car_id', $request->car_id)->first();
+        if ($carServicing) {
+            $carServicingJobs = $carServicing->servicingJobs;
+            return ok('History', $carServicingJobs);
+        }
+
+        return error('No Any Services Found.');
+    }
+
+
+    public function getStatus(Request $request)
+    {
+        $validation = Validator::make($request->all(), [
+            'car_servicing_id'  => 'required|exists:car_servicings,id'
+        ]);
+
+        if ($validation->fails())
+            return error('Validation Error', $validation->errors(), 'validation');
+
+        $carServicing = CarServicing::where('id', $request->car_servicing_id)->first();
+        $car = Car::where('id', $carServicing->car_id)->where('owner_id', auth()->user()->id)->first();
+
+        if ($car) {
+            $carServicingJob = CarServicingJob::where('car_servicing_id', $carServicing->id)->first();
+            $mechanic = User::where('id', $carServicingJob->mechanic_id)->first();
+            return response()->json([
+                'message'   => 'status',
+                'mechanic'  => $mechanic,
+                'status'    => $carServicingJob->status
+            ]);
+        }
+
+        return error('No Record Found');
     }
 }
