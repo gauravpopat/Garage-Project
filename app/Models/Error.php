2c1a8766a8c58fc6ade3a02e2f7d3759cea61727
Gaurav <gauravp@zignuts.com>
2023-04-17 17:15:35 +0530

    error reporting

---
+++ app/Models/Error.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Error extends Model
+{
+   protected $connection = 'error_mysql';
+  
+   /**
+    * The table associated with the model.
+    *
+    * @var string
+    */
+   protected $table = 'errors';
+
+
+   /**
+    * The attributes that are mass assignable.
+    *
+    * @var array
+    */
+   protected $fillable = ['user_id', 'code', 'file', 'line', 'message', 'trace'];
+}
+
